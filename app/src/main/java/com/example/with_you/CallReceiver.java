9817b708a79472a7d4297e400362ff5ea063b07c
achchu <vpachchu27@gmail.com>
2021-11-21 23:52:08 +0530

    with_you

--- app/src/main/java/com/example/with_you/CallReceiver.java
+++ app/src/main/java/com/example/with_you/CallReceiver.java
@@ -1,12 +1,20 @@
 package com.example.with_you;
+import android.content.res.AssetFileDescriptor;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.provider.Telephony;
 import android.telecom.TelecomManager;
 import android.telephony.TelephonyManager;
@@ -16,26 +24,65 @@ import android.widget.Toast;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.io.IOException;
+
 public class CallReceiver extends BroadcastReceiver {
 
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        try {
+            String state=intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+            if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
 
-        if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_OFFHOOK))
-        {
+                showToast(context, "Call Started...");
 
-            showToast(context,"Call Started...");
-        }
-        else  if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_IDLE))
-        {
-            showToast(context,"Call Ended...");
+                final Handler handler = new Handler(Looper.getMainLooper());
 
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+
+                        if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                            showToast(context, "...");
+
+
+                        }
+                        else
+                        {
+                            showToast(context, "Call Answered...");
+                            Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+// Vibrate for 500 milliseconds
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+                            } else {
+                                //deprecated in API 26
+                                v.vibrate(5000);
+                            }
+
+
+
+
+                        }
+                    }
+                }, 45000);
+
+
+
+
+            }
+            else if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                showToast(context, "Call Ringing..");
+            }
+            else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                showToast(context, "Call Ended...");}
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        else if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING))
-        {
-            showToast(context,"Call Ringing..");
-        }
+
+
     }
 
     void showToast(Context context,String message)
